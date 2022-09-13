709804e90925aaf4a7b1483588ff5058164113f6
leandro lima <leandro.desouzalima1@gmail.com>
2022-09-13 16:20:41 -0300

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning up database..."
+Manager.destroy_all
+Client.destroy_all
+puts "Database cleaned"
+puts "Creating manager"
+
+Manager.create!(
+  email: 'email@email.com',
+  password: '123456',
+  password_confirmation: '123456'
+)
+
+puts "Manager created"
+puts "Creating clients"
+
+10.times do
+  Client.create!(
+    name: Faker::Name.name,
+    account: '123456',
+    password: '123456',
+    email: 'email@email.com',
+    message: 'a random message for you'
+  )
+end
+
+puts "Clients created"
